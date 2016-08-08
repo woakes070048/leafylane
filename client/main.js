be00ed45c941fd29c67c873f78cb254dc17d6953
Tom Cusack <tom.cusack@actual-experience.com>
2016-08-08 16:38:43 +0100

    Project reshuffle

--- client/main.js
+++ client/main.js
@@ -1,25 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import ImageList from './components/ImageList';
-import axios from 'axios';
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = { images: [] };
-  }
-
-  componentWillMount() {
-    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-      .then(response => this.setState({ images: response.data.data }));
-  }
-
-  render() {
-    return (<div><ImageList images={this.state.images} /></div>);
-  }
-}
+const App = () => {
+  return (<div>Tom</div>);
+};
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
