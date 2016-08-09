6755d00729674e4a8749f5a07067b5b8b0a8df61
Tom Cusack <tom@tom-cusack.com>
2016-08-09 15:13:13 +0100

    Finally cleared all eslint errors! :sparkles:

--- client/main.js
+++ client/main.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import EmployeeList from './components/EmployeeList';
 
-const App = () => {
-  return (<div><EmployeeList /></div>);
-};
+function App() {
+  return <EmployeeList />;
+}
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
