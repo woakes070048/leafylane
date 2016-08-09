09b3f65e43ca59298bda180cab07e6e366551519
Tom Cusack <tom.cusack@actual-experience.com>
2016-08-09 11:39:33 +0100

    Added a load more button

--- client/components/EmployeeList.js
+++ client/components/EmployeeList.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees.js';
 import EmployeeDetail from './EmployeeDetail';
 
-const EmployeeList = (props) => {
-  // Access our data on props.
-  // i.e. props.employees = an array of employee objects
-  return (
-    <div>
-      <div className="employee-list">
-        {props.employees.map(employee =>
-          <EmployeeDetail
-            key={employee._id}
-            employee={employee}
-          />
-        )}
+// Default number of records subscribed per page
+const PER_PAGE = 20;
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <div>
+        <div className="employee-list">
+          {this.props.employees.map(employee =>
+            <EmployeeDetail
+              key={Math.random()}
+              employee={employee}
+            />
+          )}
+        </div>
+        <button
+          onClick={() => Meteor.subscribe('employees', 40)}
+          className="btn btn-primary"
+        >
+          Load more ..
+        </button>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+EmployeeList.propTypes = {
+  employees: React.PropTypes.array,
+  employee: React.PropTypes.object,
+  _id: React.PropTypes.object,
 };
 
 export default createContainer(() => {
   // Set up a subscription, then return an object. Whatever we
   // return will be sent to EmployeeList as a prop
-  Meteor.subscribe('employees');
+  Meteor.subscribe('employees', PER_PAGE);
   return { employees: Employees.find({}).fetch() };
 }, EmployeeList);
