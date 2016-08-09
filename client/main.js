e5b2283537e959d2f470a1c97e88d135739d2fab
Tom Cusack <tom@tom-cusack.com>
2016-08-09 16:19:51 +0100

    Starting afresh

--- client/main.js
+++ client/main.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import EmployeeList from './components/EmployeeList';
 
 function App() {
-  return <EmployeeList />;
+  return <div>Hello</div>;
 }
 
 Meteor.startup(() => {
--- imports/collections/employees.js
+++
@@ -1,3 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-
-export const Employees = new Mongo.Collection('employees');
