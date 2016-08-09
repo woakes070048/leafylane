feb1085f2e6f85eb4dad603076b8d5d11973a3df
Tom Cusack <tom.cusack@actual-experience.com>
2016-08-09 11:05:00 +0100

    Redesigned with Bootstrap :+1:

--- client/components/EmployeeList.js
+++ client/components/EmployeeList.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees.js';
+import EmployeeDetail from './EmployeeDetail';
 
 const EmployeeList = (props) => {
   // Access our data on props.
   // i.e. props.employees = an array of employee objects
-  console.log(props.employees);
   return (
     <div>
       <div className="employee-list">
-        employee-list
+        {props.employees.map(employee =>
+          <EmployeeDetail
+            key={employee._id}
+            employee={employee}
+          />
+        )}
       </div>
     </div>
   );
